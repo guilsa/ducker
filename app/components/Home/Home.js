1aa65ba1e9be99fbcf77038ecbd965ad8b9bae11
Guilherme Sa <guilsa001@gmail.com>
2016-04-27 19:42:46 -0700

    Add styling to Home component

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { PropTypes } from 'react'
-import { container, title } from './styles.css'
+import { container, title, header, subHeader } from './styles.css'
 
 export default function Home (props) {
   return (
     <div className={container}>
-      <p className={title}>{'Home'}</p>
+      <p className={title}>{'Ducker'}</p>
+      <p className={header}>Wanna see a Twitter remake?</p>
+      <p className={subHeader}>Try it out!</p>
     </div>
   )
 }
--- app/containers/main/MainContainer.js
+++ app/containers/main/MainContainer.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { container, innerContainer } from './styles.css'
 
 const MainContainer = React.createClass({
   render () {
     return (
-      <div>
-        {this.props.children}
+      <div className={container}>
+        <div className={innerContainer}>
+          {this.props.children}
+        </div>
       </div>
     )
   }
