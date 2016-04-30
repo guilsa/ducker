761fd538ed006164b8dbe933eb42abe1bd98c151
Guilherme Sa <guilsa001@gmail.com>
2016-04-30 13:12:44 -0700

    Install redux, implement store

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import ReactDOM from 'react-dom'
 import routes from './config/routes'
+import { createStore } from 'redux'
+import users from 'redux/modules/users'
+
+const store = createStore(users)
 
 ReactDOM.render(
   routes,
