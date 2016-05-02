7e6796e0728fb0d57f8d2f380e5fe1543ac17a8c
Guilherme Sa <guilsa001@gmail.com>
2016-05-02 09:14:57 -0700

    Add /feed route

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Router, hashHistory, IndexRoute, Route } from 'react-router'
-import { MainContainer, HomeContainer, AuthenticateContainer } from 'containers'
+import { MainContainer, HomeContainer, AuthenticateContainer, FeedContainer } from 'containers'
 
 const routes = (
   <Router history={hashHistory}>
     <Router path='/' component={MainContainer}>
         <Route path='auth' component={AuthenticateContainer} />
+        <Route path='feed' component={FeedContainer} />
         <IndexRoute component={HomeContainer} />
     </Router>
   </Router>
--- app/containers/Feed/FeedContainer.js
+++ app/containers/Feed/FeedContainer.js
@@ -3,7 +3,9 @@ import { Feed } from 'components'
 
 const FeedContainer = React.createClass({
   render () {
-    <Feed />
+    return (
+      <Feed />
+    )
   }
 })
 
