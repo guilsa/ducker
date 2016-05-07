c332e5f8f6c18e9e830881581ca25ca4b83b89a7
Guilherme Sa <guilsa001@gmail.com>
2016-05-07 12:04:41 -0700

    Swap mock auth with real auth

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -1,17 +1,31 @@
+import { ref } from 'config/constants'
+import { authUser, fetchingUserSuccess } from 'redux/modules/users'
+import { formatUserInfo } from 'helpers/utils'
+
 export default function auth () {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => resolve({
-      name: 'Tyler McGinnis',
-      avatar: 'https://pbs.twimg.com/profile_images/378800000605536525/891a881bde93a1fc3e289528fb859b96_400x400.jpeg',
-      uid: 'the-uid'
-    }), 2000)
-  })
+  return ref.authWithOAuthPopup('facebook')
 }
 
 export function checkIfAuthed (store) {
-  return store.getState().isAuthed
+  const authData = ref.getAuth()
+  if (authData === null) {
+    return false
+  } else if ( store.getState().isAuthed === false) {
+    const { uid, facebook } = authData
+    const userInfo = formatUserInfo(facebook.displayName, facebook.profileImageURL, uid)
+    store.dispatch(authUser(uid))
+    store.dispatch(fetchingUserSuccess(uid, userInfo, Date.now()))
+  }
+  return true
 }
 
 export function logout () {
-  console.log('Logged Out!')
+  ref.unauth()
+}
+
+export function saveUser (user) {
+  console.log(user);
+  return ref.child(`users/${user.uid}`)
+    .set(user)
+    .then(() => user)
 }
