436700f6c27ad4c68c74c914fad75be78846d377
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 13:49:22 -0700

    Encapsulate all invokations to async calls

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -4,4 +4,10 @@ function saveToUsersDucks (duck, duckId) {}
 
 function saveLikeCount (duckId) {}
 
-function saveDuck (duck) {}
+function saveDuck (duck) {
+  return Promise.all([
+    saveToDucks(duck),
+    saveToUsersDucks(duck, duckId),
+    saveLikeCount(duckId),
+  ]).then(() => ({...duck, duckId}))
+}
