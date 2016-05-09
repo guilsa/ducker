a9e2eafc079b7e3dac03f5d7a6b5a9f7c006b739
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 15:00:37 -0700

    Add ducks/twit reducer

--- app/redux/modules/ducks.js
+++ app/redux/modules/ducks.js
@@ -37,3 +37,45 @@ function addMultipleDucks (ducks) {
     ducks,
   }
 }
+
+const initialState = {
+  isFetching: true,
+  error: '',
+}
+
+function ducks (state = initialState, action) {
+  switch (action.type) {
+    case FETCHING_DUCK :
+      return {
+        ...state,
+        isFetching: true,
+      }
+    case ADD_DUCK :
+    case FETCHING_DUCK_ERROR :
+      return {
+        ...state,
+        isFetching: false,
+        error: action.error,
+      }
+    case FETCHING_DUCK_SUCCESS :
+      return {
+        ...state,
+        error: '',
+        isFetching: false,
+        [action.duck.duckId]: action.duck,
+      }
+    case REMOVE_FETCHING :
+      return {
+        ...state,
+        error: '',
+        isFetching: false,
+      }
+    case ADD_MULTIPLE_DUCKS :
+      return {
+        ...state,
+        ...action.ducks,
+      }
+    default :
+      return state
+  }
+}
