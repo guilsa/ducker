286f3d53ab338a2693ffe802adef0c9808bdd9ee
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 15:11:07 -0700

    Wrap actions in action creators

--- app/redux/modules/usersDucks.js
+++ app/redux/modules/usersDucks.js
@@ -1,22 +1,38 @@
-{
-  type: FETCHING_USERS_DUCKS,
-  error: 'Error fetching Users Duck Ids',
+function addSingleUsersDuck (uid, duckId) {
+  return {
+    type: 'ADD_SINGLE_USERS_DUCK',
+    uid,
+    duckId,
+  }
 }
 
-{
-  type: FETCHING_USERS_DUCKS_ERROR,
-  uid,
+function fetchingUsersDucks (error) {
+  return {
+    type: FETCHING_USERS_DUCKS,
+    error: 'Error fetching Users Duck Ids',
+  }
 }
 
-{
-  type: FETCHING_USERS_DUCKS_SUCCESS,
-  uid,
-  duckIds,
-  lastUpdated,
+function fetchingUsersDucksError (uid) {
+  return {
+    type: FETCHING_USERS_DUCKS_ERROR,
+    uid,
+  }
 }
 
-{
-  type: ADD_SINGLE_USERS_DUCK,
-  uid,
-  duckId,
+function fetchingUsersDucksSuccess (uid, ducksIds, lastUpdated) {
+  return {
+    type: FETCHING_USERS_DUCKS_SUCCESS,
+    uid,
+    duckIds,
+    lastUpdated,
+  }
+}
+
+function fetchingUsersDucks (uid, duckId) {
+  return {
+    type: ADD_SINGLE_USERS_DUCK,
+    uid,
+    duckId,
+  }
 }
