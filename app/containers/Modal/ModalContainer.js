ea3e8963e42443bb08f585671b857e5818f7ace2
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 10:11:06 -0700

    Map state to modal props

--- app/containers/Modal/ModalContainer.js
+++ app/containers/Modal/ModalContainer.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Modal } from 'components'
+import { connect } from 'react-redux'
 
 const ModalContainer = React.createClass({
   render () {
     return (<Modal />)
   },
 })
+
+function mapStateToProps ({modal, users}) {
+  const duckTextLength = modal.duckText.length
+  return {
+    user: users[users.authedId] ? users[users.authedId.info] : {},
+    duckText: modal.duckText,
+    isOpen: modal.isOpen,
+    isSubmitDisabled: duckTextLength <= 0 || duckTextLength < 140,
+  }
+}
 
 export default ModalContainer
