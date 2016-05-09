fe3119aed5b44faac8df4dc16b5e81498b039afc
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 10:09:08 -0700

    Add modal UI

--- app/components/Modal/Modal.js
+++ app/components/Modal/Modal.js
@@ -4,6 +4,17 @@ import {
   newDuckTop, pointer, newDuckInputContainer,
   newDuckInput, submitDuckBtn, darkBtn } from './styles.css'
 
+const modalStyles = {
+  content: {
+    width: 350,
+    margin: '0px auto',
+    height: 220,
+    borderRadius: 5,
+    background: '#EBEBEB',
+    padding: 0,
+  },
+}
+
 const { object, string, func, bool } = PropTypes
 Modal.PropTypes = {
   duckText: string.isRequired,
@@ -16,7 +27,35 @@ Modal.PropTypes = {
 }
 
 export default function Modal (props) {
+  function submitDuck () {
+    console.log('Duck', props.duckText)
+    console.log('User', props.user)
+  }
+
   return (
-    <div>Modal</div>
+    <span className="{darkBtn}" onClick={props.openModal}>
+      {'Duck'}
+      <ReactModal style={modalStyles} isOpen={props.isOpen} onRequestClose={props.closeModal}>
+        <div className={newDuckTop}>
+          <span>{'Compose new Duck'}</span>
+          <span onClick={props.closeModal} className={pointer}>{'X'}</span>
+        </div>
+        <div className={newDuckInputContainer}>
+          <textarea
+            onChange={(e) => props.updateDuckText(e.target.value)}
+            value={props.duckText}
+            maxLength={140}
+            type='text'
+            className={newDuckInput}
+            placeholder="What's on your mind?" />
+        </div>
+        <button
+          className={submitDuckBtn}
+          disabled={props.isSubmitDisabled}
+          onClick={submitDuck}>
+            {'Duck'}
+        </button>
+      </ReactModal>
+    </span>
   )
 }
