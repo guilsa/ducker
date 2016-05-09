80bf7ecebfc7401d4a60f5464f3452b478188d41
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 10:17:15 -0700

    Import and bind action creators

--- app/containers/Modal/ModalContainer.js
+++ app/containers/Modal/ModalContainer.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
 import { Modal } from 'components'
 import { connect } from 'react-redux'
+import * as modalActionCreators from 'redux/modules/modal'
 
 const ModalContainer = React.createClass({
   render () {
@@ -16,6 +18,10 @@ function mapStateToProps ({modal, users}) {
     isOpen: modal.isOpen,
     isSubmitDisabled: duckTextLength <= 0 || duckTextLength < 140,
   }
+}
+
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(modalActionCreators, dispatch)
 }
 
 export default ModalContainer
