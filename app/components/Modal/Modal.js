859dc5a99f0e330b8e9bf302bd72dc84edf9c3da
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 00:01:24 -0700

    Add Modal component

---
+++ app/components/Modal/Modal.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { default as ReactModal } from 'react-modal'
+import {
+  newDuckTop, pointer, newDuckInputContainer,
+  newDuckInput, submitDuckBtn, darkBtn } from './styles.css'
+
+export default function Modal (props) {
+  return (
+    <div>Modal</div>
+  )
+}
---
+++ app/containers/Modal/ModalContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Modal } from 'components'
+
+const ModalContainer = React.createClass({
+  render () {
+    return (<Modal />)
+  },
+})
+
+export default ModalContainer
