26bb8dfe7d81c3d87f0b91292b910f6914c67196
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 14:06:30 -0700

    Complete dependencies, get a hold of db obj

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -4,6 +4,16 @@ function saveToDucks (duck) {
   const duckId = ref.child('ducks').push().key() // not pushing anything,
                                                 // returns a reference to
                                                 // specific endpoint
+    // spread operator creates a brand new obj
+    // which has all of the properties of duck
+    // as well as our new duckId on it.
+    // So it adds duckId to the object.
+  const duckPromise = ref.child(`ducks/${duckId}`).set({...duck, duckId})
+
+  return {
+    duckId,
+    duckPromise
+  }
 }
 
 function saveToUsersDucks (duck, duckId) {}
