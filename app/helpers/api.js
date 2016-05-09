8a604e689b03a16e14f5b6b91c32f819ca8c2df4
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 13:54:23 -0700

    Generate unique id for data persistence

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,12 +1,20 @@
-function saveToDucks (duck) {}
+import { ref } from 'config/constants'
+
+function saveToDucks (duck) {
+  const duckId = ref.child('ducks').push().key() // not pushing anything,
+                                                // returns a reference to
+                                                // specific endpoint
+}
 
 function saveToUsersDucks (duck, duckId) {}
 
 function saveLikeCount (duckId) {}
 
 function saveDuck (duck) {
+  const { duckId, duckPromise } = saveToDucks(duck)
+
   return Promise.all([
-    saveToDucks(duck),
+    duckPromise,
     saveToUsersDucks(duck, duckId),
     saveLikeCount(duckId),
   ]).then(() => ({...duck, duckId}))
