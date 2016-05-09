a2a60fa8293b624575cd7058067383b79621a92e
Guilherme Sa <guilsa001@gmail.com>
2016-05-09 14:47:12 -0700

    Add ducks/twits actions

---
+++ app/redux/modules/ducks.js
@@ -0,0 +1,27 @@
+{
+  type: FECTHING_DUCK,  
+}
+
+{
+  type: FETCHING_DUCK_ERROR,
+  error: 'Error fetching Duck',
+}
+
+{
+  type: FETCHING_DUCK_SUCCESS,
+  duck,
+}
+
+{
+  type: REMOVE_FETCHING,
+}
+
+{
+  type: ADD_DUCK,
+  duck,
+}
+
+{
+  type: ADD_MULTIPLE_DUCKS,
+  ducks,
+}
