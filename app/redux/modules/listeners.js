4cecc28e7a51d68e101efb880c090e5f0c8c8e81
Guilherme Sa <guilsa001@gmail.com>
2016-05-10 08:11:06 -0700

    Add listener action creator

--- app/redux/modules/listeners.js
+++ app/redux/modules/listeners.js
@@ -1,3 +1,12 @@
+const ADD_LISTENER = 'ADD_LISTENER'
+
+function addListener (listenerId) {
+  return {
+    ADD_LISTENER,
+    listenerId
+  }
+}
+
 export default function listeners (state = {}, action) {
   switch (action.type) {
     case ADD_LISTENER :
